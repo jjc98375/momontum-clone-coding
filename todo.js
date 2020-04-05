130d63619a7aa16932c6f91b29c95442da531327
jjc98375 <collegecho98@gmail.com>
2020-04-04 22:15:35 -0700

    summary on saturday april 4th
    temporarily take rest

---
+++ todo.js
@@ -0,0 +1,15 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = form.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+}
+
+function init() {
+  loadToDos();
+}
+
+init();
